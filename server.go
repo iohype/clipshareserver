a42f4355a28e2679c5ec1b103e0d80f80b53d808
Faithfulness Alamu <vaguemail369@gmail.com>
2020-10-02 11:39:49 +0100

    Make server a http handler
    - Implement the ServeHTTP method in server
    - Delegate to the router

--- server.go
+++ server.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/gorilla/mux"
+	"net/http"
 )
 
 type server struct{
@@ -13,4 +14,8 @@ func newServer() *server {
 	srv.router = mux.NewRouter()
 	srv.setupRoutes(srv.router)
 	return srv
+}
+
+func (s *server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	s.router.ServeHTTP(w, r)
 }
