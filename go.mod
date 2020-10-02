33a2afed05e9099e88827a21db7334e7d4a13165
Faithfulness Alamu <vaguemail369@gmail.com>
2020-10-02 11:31:15 +0100

    Create server method to setup routes
    This method uses gorilla mux for routing

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/iohype/clipshareserver
 
 go 1.15
+
+require github.com/gorilla/mux v1.8.0
---
+++ routes.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/gorilla/mux"
+
+func (s *server) setupRoutes(r *mux.Router) {
+
+}
