30024f2113eda587e7d016efbbc2da3ee8ce819f
Faithfulness Alamu <vaguemail369@gmail.com>
2020-10-02 11:34:02 +0100

    Setup routes when creating new server
    - Add a router as a server dependency
    - Init the router when creating new server
    - Register paths on the router

--- server.go
+++ server.go
@@ -1,8 +1,16 @@
 package main
 
-type server struct{}
+import (
+	"github.com/gorilla/mux"
+)
+
+type server struct{
+	router *mux.Router
+}
 
 func newServer() *server {
 	srv := &server{}
+	srv.router = mux.NewRouter()
+	srv.setupRoutes(srv.router)
 	return srv
 }
