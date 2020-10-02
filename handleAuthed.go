f880d12f586bdd9823e7bf2324f6b9ceb529cb88
Faithfulness Alamu <vaguemail369@gmail.com>
2020-10-02 12:15:38 +0100

    Implement the handleAuthed middleware
    - Create a protected cache for idToken to uid conversion
    - Retrieve the idToken from the Authorization header
    - Verify token from firebase if not in cache
    - Verified tokens allow request to go through

--- handleAuthed.go
+++ handleAuthed.go
@@ -2,13 +2,39 @@ package main
 
 import (
 	"net/http"
+	"strings"
+	"sync"
 )
 
 //handleAuthed is a middleware that allows the request only if user is logged in
 func (s *server) handleAuthed(next http.HandlerFunc) http.HandlerFunc {
-
+	// Cache hit prevents asking FirebaseAuth to verify id token
+	var (
+		srw sync.RWMutex
+		authedCache = make(map[string]string)
+	)
 	return func(w http.ResponseWriter, r *http.Request) {
-
-		//next(w, r)
+		idToken := r.Header.Get("Authorization")
+		idToken = strings.TrimPrefix("Bearer", idToken)
+		idToken = strings.TrimSpace(idToken)
+		srw.RLock()
+		_, ok := authedCache[idToken]
+		srw.RUnlock()
+		if !ok {
+			// idToken not in cache, try firebase auth
+			uid, err := verifyToken(idToken)
+			if err != nil {
+				s.Error(w, r, err, http.StatusUnauthorized)
+				return
+			}
+			srw.Lock()
+			authedCache[idToken] = uid
+			srw.Unlock()
+		}
+		next(w, r)
 	}
+}
+
+func verifyToken(idToken string) (string, error) {
+	return "", nil
 }
