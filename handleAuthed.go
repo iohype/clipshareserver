e531059666b1d72e04e382ad219b8de42f519f8d
Faithfulness Alamu <vaguemail369@gmail.com>
2020-10-04 21:04:57 +0100

    Refactor authentication middleware

--- handleAuthed.go
+++ handleAuthed.go
@@ -8,9 +8,50 @@ import (
 	"sync"
 )
 
-// Key to retrieve uid from a context
+// Key used to retrieve uid from context
 type uidKey struct{}
 
+//handleAuthed is a middleware that allows the request only if user is logged in
+func (s *server) handleAuthed(next http.HandlerFunc) http.HandlerFunc {
+	// Cache hit prevents asking FirebaseAuth to verify id token
+	var (
+		mu          sync.RWMutex
+		authedCache = make(map[string]string)
+	)
+	return func(w http.ResponseWriter, r *http.Request) {
+		idToken := tokenFromHeader(r)
+		// Read from cache
+		mu.RLock()
+		uid, ok := authedCache[idToken]
+		mu.RUnlock()
+		if !ok {
+			// token not in cache, try FirebaseAuth
+			// err is declared this way here to avoid creating a new uid variable
+			//in this if block due to the use of the := shortcut
+			var err error
+			uid, err = verifyToken(idToken)
+			if err != nil {
+				s.Error(w, r, err, http.StatusUnauthorized)
+				return
+			}
+			// Save to cache
+			mu.Lock()
+			authedCache[idToken] = uid
+			mu.Unlock()
+		}
+		// Pass the uid down to handlers
+		ctx := s.uidInContext(r.Context(), uid)
+		next(w, r.WithContext(ctx))
+	}
+}
+
+//uidInContext returns a context cloned from the baseCtx context
+//but with uidKey{} set to value uid
+func (s *server) uidInContext(baseCtx context.Context, uid string) context.Context {
+	return context.WithValue(baseCtx, uidKey{}, uid)
+}
+
+//uidFromContext returns the uid from ctx context
 func (s *server) uidFromContext(ctx context.Context) (string, error) {
 	val, ok := ctx.Value(uidKey{}).(string)
 	if !ok {
@@ -19,35 +60,11 @@ func (s *server) uidFromContext(ctx context.Context) (string, error) {
 	return val, nil
 }
 
-//handleAuthed is a middleware that allows the request only if user is logged in
-func (s *server) handleAuthed(next http.HandlerFunc) http.HandlerFunc {
-	// Cache hit prevents asking FirebaseAuth to verify id token
-	var (
-		srw         sync.RWMutex
-		authedCache = make(map[string]string)
-	)
-	return func(w http.ResponseWriter, r *http.Request) {
-		idToken := r.Header.Get("Authorization")
-		idToken = strings.TrimPrefix("Bearer", idToken)
-		idToken = strings.TrimSpace(idToken)
-		srw.RLock()
-		uid, ok := authedCache[idToken]
-		srw.RUnlock()
-		if !ok {
-			// idToken not in cache, try firebase auth
-			uid, err := verifyToken(idToken)
-			if err != nil {
-				s.Error(w, r, err, http.StatusUnauthorized)
-				return
-			}
-			srw.Lock()
-			authedCache[idToken] = uid
-			srw.Unlock()
-		}
-		// Pass the uid down
-		ctx := context.WithValue(r.Context(), uidKey{}, uid)
-		next(w, r.WithContext(ctx))
-	}
+func tokenFromHeader(r *http.Request) string {
+	idToken := r.Header.Get("Authorization")
+	idToken = strings.TrimPrefix(idToken, "Bearer")
+	idToken = strings.TrimSpace(idToken)
+	return idToken
 }
 
 func verifyToken(idToken string) (string, error) {
