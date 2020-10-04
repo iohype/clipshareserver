1841a0bb982020af8c0aa81914772aa8cc127398
Faithfulness Alamu <vaguemail369@gmail.com>
2020-10-04 17:06:21 +0100

    Reduce database interface methods
    Leave only essential methods

--- db.go
+++ db.go
@@ -6,10 +6,6 @@ type DB interface {
 	Get(userID string) ([]Clip, error)
 	// get all user clips since timestamp
 	GetSince(userId string, timestamp string) ([]Clip, error)
-	// get single clip
-	GetClip(userId string, clipID string) (Clip, error)
 	// save a clip
 	Put(userID string, clip Clip) error
-	// save multiple clips
-	PutAll(userID string, clips []Clip) error
 }
