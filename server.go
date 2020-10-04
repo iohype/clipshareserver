f4a6c1c9c31904aae2f801a7ec5e4fc254443bdb
Faithfulness Alamu <vaguemail369@gmail.com>
2020-10-04 18:21:30 +0100

    Init defaults for server deps
    - Use default server handler
    - Create a default logger

--- server.go
+++ server.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 )
 
 type option func(*server)
@@ -17,6 +18,10 @@ type server struct {
 //newServer returns a server after applying all options
 func newServer(opts ...option) *server {
 	srv := &server{}
+	// Init defaults
+	srv.handler = srv.routes()
+	srv.logger = log.New(os.Stdout, "", log.LstdFlags)
+	// Apply all options
 	for _, opt := range opts {
 		opt(srv)
 	}
