c471015366ddb41e41bf271b67da30aa040f7e3c
Faithfulness Alamu <vaguemail369@gmail.com>
2020-10-04 20:41:36 +0100

    Implement handler for getting clips

--- handleClipsGet.go
+++ handleClipsGet.go
@@ -1,12 +1,29 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 )
 
 func (s *server) handleClipsGet() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		s.Error(w, r, fmt.Errorf("not implemented"), http.StatusNotImplemented)
+		since := r.URL.Query().Get("since")
+		// Get userID from context
+		userID, err := s.uidFromContext(r.Context())
+		if err != nil {
+			s.Error(w, r, err, http.StatusInternalServerError)
+		}
+		var clips []Clip
+		if since != "" {
+			s.logger.Printf("Getting clips for uid %s since %s\n", userID, since)
+			clips, err = s.db.GetSince(userID, since)
+		} else {
+			s.logger.Printf("Getting all clips for uid: %s", userID)
+			clips, err = s.db.Get(userID)
+		}
+		if err != nil {
+			s.Error(w, r, err, http.StatusInternalServerError)
+			return
+		}
+		s.JSON(w, r, clips, http.StatusOK)
 	}
 }
