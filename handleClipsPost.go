cfdf6503205642f7f30a15f4853284e980b9720f
youthtrouble <harjhibolar@gmail.com>
2020-10-07 21:11:24 +0100

    changes effected

--- handleClipsPost.go
+++ handleClipsPost.go
@@ -14,16 +14,20 @@ func (s *server) handleClipsPost(w http.ResponseWriter, r *http.Request) {
 	err := json.NewDecoder(r.Body).Decode(&clip)
 	if err != nil {
 		// If there is something wrong with the request body, return a 400 status
-		s.Error(w, r, err, 400)
+		s.Error(w, r, err, http.StatusBadRequest)
 	}
 
-	uid := clip.ID
+	uid, err := s.uidFromContext(r.Context())
+	if err != nil {
+		s.Error(w, r, err, http.StatusInternalServerError)
+		return
+	}
+	
 	clip.Timestamp = time.Now().UnixNano() 
 
 	if err = s.db.Put(uid, clip); err != nil {
 		// If there is any issue with inserting into the database, return a 500 error
-		s.Error(w, r, err, 500)
+		s.Error(w, r, err, http.StatusInternalServerError)
 	}
-	w.WriteHeader(http.StatusCreated)
-	json.NewEncoder(w).Encode(clip)
+	s.JSON(w, r, clip, http.StatusCreated) 
 }
