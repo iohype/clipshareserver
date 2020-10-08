8e638bf00700f5aad5c19d5d890ca6ea24c62926
Faithfulness Alamu <vaguemail369@gmail.com>
2020-10-08 16:55:21 +0100

    Fix superfluous output in handleClipsPost handler

--- handleClipsPost.go
+++ handleClipsPost.go
@@ -6,7 +6,6 @@ import (
 	"time"
 )
 
-
 func (s *server) handleClipsPost(w http.ResponseWriter, r *http.Request) {
 	// Parse and decode the request body into a new `Clip` instance
 	var clip Clip 
@@ -15,6 +14,7 @@ func (s *server) handleClipsPost(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		// If there is something wrong with the request body, return a 400 status
 		s.Error(w, r, err, http.StatusBadRequest)
+		return
 	}
 
 	uid, err := s.uidFromContext(r.Context())
@@ -28,6 +28,7 @@ func (s *server) handleClipsPost(w http.ResponseWriter, r *http.Request) {
 	if err = s.db.Put(uid, clip); err != nil {
 		// If there is any issue with inserting into the database, return a 500 error
 		s.Error(w, r, err, http.StatusInternalServerError)
+		return
 	}
 	s.JSON(w, r, clip, http.StatusCreated) 
 }
