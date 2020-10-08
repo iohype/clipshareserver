82870880e69020e418f0a0fcb6017475224acc08
Faithfulness Alamu <vaguemail369@gmail.com>
2020-10-08 16:59:35 +0100

    Add clip creation handler to server routes
    - Rename handleClipsPost to handleClipsCreate
    - Use anonymous function in handleClipsCreate
    - Register handleClipsCreate in server handler

---
+++ handleClipsCreate.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+	"time"
+)
+
+func (s *server) handleClipsCreate() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		// Parse and decode the request body into a new `Clip` instance
+		var clip Clip
+
+		err := json.NewDecoder(r.Body).Decode(&clip)
+		if err != nil {
+			// If there is something wrong with the request body, return a 400 status
+			s.Error(w, r, err, http.StatusBadRequest)
+			return
+		}
+
+		uid, err := s.uidFromContext(r.Context())
+		if err != nil {
+			s.Error(w, r, err, http.StatusInternalServerError)
+			return
+		}
+
+		clip.Timestamp = time.Now().UnixNano()
+
+		if err = s.db.Put(uid, clip); err != nil {
+			// If there is any issue with inserting into the database, return a 500 error
+			s.Error(w, r, err, http.StatusInternalServerError)
+			return
+		}
+		s.JSON(w, r, clip, http.StatusCreated)
+	}
+}
